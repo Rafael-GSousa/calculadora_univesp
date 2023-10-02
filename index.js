8631e4a5d8f3dfa06cab2f50a07e30303dc356d0
Rafael Galdino <88910477+Rafael-GSousa@users.noreply.github.com>
2023-10-02 04:07:39 -0300

    update project

--- index.js
+++ index.js
@@ -1,85 +1,50 @@
-const iAva = document.querySelector('#iAva');
-// const iProva = document.querySelector('#iProva');
-const mediaAva = document.querySelector('#mediaAva');
-// const mediaProva = document.querySelector('#mediaProva');
-// const iMedia = document.querySelector('#iMedia');
-// const parcialFinal = document.querySelector('#parcialFinal');       
-// const iStatus = document.querySelector('#iStatus');
-// const valida = document.querySelector('#valida');
-// const final = document.querySelector('#final');
-// const media = Number(iAva.value * 0.4) + Number(iProva.value * 0.6);
+const nAva = document.querySelector('#nAva');
+const nProva = document.querySelector('#nProva');
+const nExame = document.querySelector('#nExame');
+const mAva = document.querySelector('#mAva');
+const mProva = document.querySelector('#mProva');
+const container = document.querySelector('#container');
+const media = document.querySelector('#media');
+const situacao = document.querySelector('#situacao');
+const container1 = document.querySelector('#container1');
+const container2 = document.querySelector('#container2');
+const mediaFinal = document.querySelector('#mediaFinal');
+const situacaoFinal = document.querySelector('#situacaoFinal');
+let md = 0;
+let mdf = 0;
 
-// dados(iAva, iProva);
-// mediaParcial(media);
+function mediaAva(){
+    mAva.innerHTML = '==> Nota do Ava * 0.4 (40%) = ' + Number(nAva.value * 0.4);
+}
 
-// function dados(iAva, iProva) {
-    if(iAva.value != '' && iAva.value <= 10){
-        mediaAva.innerHTML = ' ==> Nota do AVA * 0.4 (40%) ==> ' + Number(iAva.value * 0.4);
-        mediaAva.style.display = 'inline-block'
-        
+function mediaProva(){
+    mProva.innerHTML = '==> Nota da Prova * 0.6 (60%) = ' + Number(nProva.value * 0.6);
+}
+
+function calcMedia(){
+    md = Number(nAva.value * 0.4) + Number(nProva.value * 0.6);
+    media.innerHTML = md;
+
+    if(md >= 5.0){
+        situacao.innerHTML = 'Essa é sua média final. Aprovado(a)!'
+        container.style.display = 'block'
+        container1.style.display = 'none';
     }else{
-        mediaAva.style.display = 'none'
+        situacao.innerHTML = 'Essa é sua média parcial. Exame!';
+        container.style.display = 'block';
+        container1.style.display = 'block';
     }
+}
 
-//     if (iProva.value != '' && iProva.value <= 10){
-//         mediaProva.innerHTML = ' ==> Nota da Prova * 0.6 (60%) ==> ' + Number(iProva.value * 0.6);
-//         mediaProva.style.display = 'inline-block'
-//     }else{
-//         mediaProva.style.display = 'none'
-//     }
-// }
+function calcMediaFinal(){
+    mdf = (md + Number(nExame.value)) / 2;
+    mediaFinal.innerHTML = mdf;
 
-// function mediaParcial(media){
-//     if(media >= 5) {
-//         parcialFinal.innerHTML = 'Final'
-//         iStatus.innerHTML = 'Aprovado(a). Parabéns!'
-//         valida.style.display = 'none'
-//     }else{
-//         valida.innerHTML = '<label for="iExame">Nota do Exame, caso já tenha: </label><input type="number" name="Exame" id="iExame" min="0" max="10">'
-//         valida.style.display = 'block'
-//         parcialFinal.innerHTML = 'Parcial'
-//         iStatus.innerHTML = 'Exame. Boa sorte!'
-//         final.style.display = 'none'
-//     }
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// mediaParcial(mP);
-
-// function mediaParcial(mP) {
-//     if (mP >= 5.0){
-//         alert(`Você tirou a nota ${mP} e foi aprovado na disciplina. Parabéns!`);
-//     }else{
-//         verificaExame();
-//     }
-// }
-
-// function verificaExame() {
-//     let resposta = String(prompt('Já tem a nota do exame? (S/N)')).toUpperCase();
-//     if (resposta === 'S'){
-//         let exame = parseFloat(prompt('Digite sua nota do Exame'));
-//         mediaFinal(exame);
-//     }else{
-//         alert(`Sua média parcial é ${mP}. Aguarde o exame e Boa sorte!`);
-//     }
-// }
-
-// function mediaFinal(exame) {
-//     let mF = (mP + exame) / 2;
-//     if (mF >= 5.0){
-//         alert(`Você tirou a média final ${mF} e foi aprovado na disciplina. Parabéns!`);
-//     }else{
-//         alert(`Sua média final é ${mF}. Infelizmente, você ficou de DP pois não atingiu a média mínima de 5.`);
-//     }
-// }
+    if(mdf >= 5.0){
+        situacaoFinal.innerHTML = 'Essa é sua média final. Aprovado(a)!'
+        container2.style.display = 'block'
+    }else{
+        situacaoFinal.innerHTML = 'Essa é sua média final. Reprovado(a)!'
+        container2.style.display = 'block'
+    }
+}
